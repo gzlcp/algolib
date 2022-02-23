41cc877885d241c8fa1efbb0f4daa6df75d2dba2
gzlcp <kyoharu213@gmail.com>
2022-02-24 00:00:51 +0900

    Fix union-find.test.cpp

--- tests/data-structures/union-find.test.cpp
+++ tests/data-structures/union-find.test.cpp
@@ -1,4 +1,5 @@
-#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
+#define PROBLEM \
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
 #include "../../data-structures/union-find.cpp"
 
 #include <iostream>
